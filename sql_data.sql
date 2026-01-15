868072ac201ae5cc9f8efa5dc0d18fa9417dcc9c
ManjunathMurari <manjunathmurari7@gmail.com>
2026-01-15 12:00:12 +0530

    Add files via upload

---
+++ sql_data.sql
@@ -0,0 +1,3 @@
+SELECT * FROM grocery.products;
+INSERT INTO products (name, uom_id, price_per_unit) VALUES ('Rice', 2, 50.0);
+INSERT INTO products (name, uom_id, price_per_unit) VALUES ('Soap', 1, 25.0);
