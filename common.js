96c6652efc59b35d7a71c7b72e8b6fc2c0c9173d
ManjunathMurari <manjunathmurari7@gmail.com>
2026-01-16 02:56:18 +0530

    Update common.js

--- common.js
+++ common.js
@@ -1,8 +1,3 @@
-// ui/js/common.js
-
-// This file stores the backend API URLs to be used across all frontend scripts.
-// If your Flask server port is different from 5000, update the port number below.
-
 var productListApiUrl = 'http://127.0.0.1:5000/getProducts';
 var uomListApiUrl = 'http://127.0.0.1:5000/getUOM';
 var productSaveApiUrl = 'http://127.0.0.1:5000/insertProduct';
