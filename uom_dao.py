d09414c061c749395d42c879398c618789350db2
ManjunathMurari <manjunathmurari7@gmail.com>
2026-01-16 02:45:35 +0530

    Update uom_dao.py

--- uom_dao.py
+++ uom_dao.py
@@ -1,9 +1,20 @@
+import mysql.connector
+
 def get_uoms(connection):
+    # 1. Check if connection is alive. If not, reconnect.
+    if not connection.is_connected():
+        connection.reconnect()
+
     cursor = connection.cursor(buffered=True)
     query = ("SELECT * FROM uom")
     cursor.execute(query)
+    
     response = []
     for (uom_id, uom_name) in cursor:
-        response.append({'uom_id': uom_id, 'uom_name': uom_name})
+        response.append({
+            'uom_id': uom_id,
+            'uom_name': uom_name
+        })
+        
     cursor.close()
     return response
