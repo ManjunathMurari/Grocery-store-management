012606e8c7d872f44310231434d417ec140cba33
ManjunathMurari <manjunathmurari7@gmail.com>
2026-01-15 11:58:42 +0530

    Add files via upload

--- sql_connection.py
+++ sql_connection.py
@@ -1,15 +1,15 @@
-
 import mysql.connector
 
-__connection = None
+__cnx = None
 
 def get_sql_connection():
-    global __connection
-    # Update password if yours is different
-    __connection = mysql.connector.connect(
-        host='127.0.0.1',
-        user='root',
-        password='Manju@123',
-        database='grocery'
-    )
-    return __connection
+    global __cnx
+    # Only create a new connection if it doesn't already exist
+    if __cnx is None:
+        __cnx = mysql.connector.connect(
+            user='root', 
+            password='root', # Replace with your MySQL password
+            host='127.0.0.1',
+            database='grocery_store'
+        )
+    return __cnx
